283c4841092119e5831331caf456c1bcc7d40a0b
Dymitr Voitenko <60138143+voitd@users.noreply.github.com>
2020-07-28 20:54:51 +0300

    refactor: resolve mergin conflict

---
+++ src/features/messages/NewMessageForm.jsx
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+import { useFormik } from 'formik';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { selectChannelId } from '../channels/channelSlice';
+import { addNewMessage } from './messageSlice';
+import { UserContext } from '../../app';
+
+const time = new Date();
+const formattedTime = time.toLocaleString('ru', { hour: 'numeric', minute: 'numeric' });
+
+const NewMessageForm = () => {
+  const dispatch = useDispatch();
+  const currentChannelId = useSelector(selectChannelId);
+  const user = useContext(UserContext);
+
+  const formik = useFormik({
+    initialValues: {
+      message: ''
+    },
+    onSubmit: (values, { resetForm }) => {
+      if (!values.message) return;
+      const message = {
+        channelId: currentChannelId,
+        timestamp: formattedTime,
+        nickname: user.name,
+        text: values.message,
+        avatar: user.avatar
+      };
+      dispatch(addNewMessage(message));
+      resetForm();
+    }
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <InputGroup className="p-2">
+        <FormControl
+          placeholder="Type message..."
+          id="message"
+          name="message"
+          onChange={formik.handleChange}
+          value={formik.values.message}
+        />
+
+        <InputGroup.Append>
+          <Button type="submit" variant="outline-info">
+            Send
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </Form>
+  );
+};
+NewMessageForm.contextType = UserContext;
+export default NewMessageForm;
