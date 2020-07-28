5d5bee9b5323edde84a1204157528d527ec41b20
Dimytr <Voitenko>
2020-07-29 00:11:53 +0300

    Merge branch 'master' of github.com:voitd/frontend-project-lvl4
    # Conflicts:
    #	.eslintrc.yml
    #	package-lock.json
    #	package.json
    #	src/app.js
    #	src/components/App.jsx
    #	src/components/Chat.jsx
    #	src/components/Sidebar.jsx
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
 
 import gon from 'gon';
-<<<<<<< HEAD
-=======
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
->>>>>>> effcfadab718bc35bbd7f87edf7b7424b84d636a
+
 import app from './app';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-<<<<<<< HEAD
-=======
-// const { channels, messages, currentChannelId } = gon;
-
->>>>>>> effcfadab718bc35bbd7f87edf7b7424b84d636a
 app(gon);
