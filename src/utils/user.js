2bdbcd6759e8a8b0d4f2829d35d5b6c3875ac48f
Dimytr <Voitenko>
2020-07-29 22:39:30 +0300

    style: formatting

--- src/utils/user.js
+++ src/utils/user.js
@@ -4,9 +4,11 @@ import faker from 'faker';
 export default () => {
   const randomName = faker.name.findName();
   const randomAvatar = faker.image.avatar();
+
   if (!Cookies.get('user')) {
     Cookies.set('user', { name: randomName, avatar: randomAvatar });
   }
+
   const user = Cookies.getJSON('user');
   return user;
 };
