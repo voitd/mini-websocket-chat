b6b166ef34550ac8faa3c3df962a3aa4a3e1c6be
Dimytr <Voitenko>
2020-08-06 01:16:16 +0300

    feat: handleSubmit implementation

--- src/features/messages/NewMessageForm.jsx
+++ src/features/messages/NewMessageForm.jsx
@@ -21,29 +21,31 @@ const NewMessageForm = () => {
     inputRef.current.focus();
   }, []);
 
+  const handleSubmit = (values, { resetForm, setSubmitting, setStatus }) => {
+    const message = {
+      channelId: currentChannelId,
+      timestamp: formattedTime,
+      nickname: name,
+      text: values.message,
+      avatar
+    };
+    dispatch(createNewMessage(message))
+      .then(unwrapResult)
+      .catch((err) => {
+        dispatch(createError(err.message));
+        setStatus(err.message);
+      })
+      .finally(() => {
+        setSubmitting(false);
+        resetForm();
+      });
+  };
+
   const formik = useFormik({
     initialValues: {
       message: ''
     },
-    onSubmit: (values, { resetForm, setSubmitting, setStatus }) => {
-      const message = {
-        channelId: currentChannelId,
-        timestamp: formattedTime,
-        nickname: name,
-        text: values.message,
-        avatar
-      };
-      dispatch(createNewMessage(message))
-        .then(unwrapResult)
-        .catch((err) => {
-          dispatch(createError(err.message));
-          setStatus(err);
-        })
-        .finally(() => {
-          setSubmitting(false);
-          resetForm();
-        });
-    }
+    onSubmit: handleSubmit
   });
 
   return (
@@ -56,11 +58,11 @@ const NewMessageForm = () => {
           ref={inputRef}
           onChange={formik.handleChange}
           value={formik.values.message}
-          isInvalid={!!formik.errors.message}
           disabled={formik.isSubmitting}
+          isInvalid={!!formik.status}
           required
         />
-        <Form.Control.Feedback type="invalid">{formik.errors.message}</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">{formik.status}</Form.Control.Feedback>
         <InputGroup.Append>
           <Button type="submit" variant="outline-info">
             Send
