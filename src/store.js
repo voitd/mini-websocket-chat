044f5e5ce638580ea0f682e257b8a6ff3c1e1f66
Dimytr <Voitenko>
2020-08-06 02:12:30 +0300

    refactor: add rootReducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import channelReducer from './features/channels/channelSlice';
-import messageReducer from './features/messages/messageSlice';
-import modalReducer from './features/modals/modalSlice';
-import alertReducer from './features/alerts/alertSlice';
+import rootReducer from './reducers';
 
 /* eslint-disable no-underscore-dangle */
 const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -11,12 +8,7 @@ const devtoolMiddleware = ext && ext();
 /* eslint-enable */
 
 const store = configureStore({
-  reducer: {
-    chat: channelReducer,
-    messages: messageReducer,
-    modal: modalReducer,
-    alerts: alertReducer
-  },
+  reducer: rootReducer,
   devtoolMiddleware
 });
 
