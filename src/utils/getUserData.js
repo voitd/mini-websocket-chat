82a14c0f847648c9e2debf6aa0da341921d31f50
Dimytr Voitenko <dimytr.voitenko@gmail.com>
2020-08-28 01:08:21 +0300

    refactor: replaced coocie get/set logic top app module

--- src/utils/getUserData.js
+++ src/utils/getUserData.js
@@ -1,13 +1,8 @@
-import Cookies from 'js-cookie';
 import faker from 'faker';
 
 export default () => {
   const randomName = faker.name.findName();
   const randomAvatar = faker.image.avatar();
-  const randomUser = { name: randomName, avatar: randomAvatar };
 
-  const user = Cookies.getJSON('user') ?? randomUser;
-  Cookies.set('user', user);
-
-  return user;
+  return { name: randomName, avatar: randomAvatar };
 };
